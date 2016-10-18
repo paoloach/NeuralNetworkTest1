42fbe02df419b4737b4b56d3d1a620f26fa162cf
paoloach <paolo.achdjian@skillbill.it>
2016-10-18 06:52:45 +0200

    work in progress

--- pollini.py
+++ pollini.py
@@ -2,8 +2,17 @@ import tensorflow as tf
 
 from scipy import misc
 import glob
+import json
+ 
 
-for image_path in glob.glob("*.png"):
+print("start")
+for image_path in glob.glob("*.jpg"):
+    dataFile = image_path[:-4]+".txt";
+    print (dataFile)
+    with open(dataFile) as f:
+        data = json.load(f);
+        print(len(data["squareYellow"]))
     image = misc.imread(image_path)
-    print image.shape
-    print image.dtype
+    print (image.shape)
+    print (image.dtype)
+    print data["squareYellow"].count()(image[0,0])
