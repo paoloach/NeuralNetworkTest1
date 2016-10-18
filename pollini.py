e11bd7c8ae31f939acae17a85e496e0f4f4ae558
Paolo Achdjian <pachdjian@gmail.com>
2016-10-18 13:11:56 +0200

    work in progress

--- pollini.py
+++ pollini.py
@@ -4,15 +4,37 @@ from scipy import misc
 import glob
 import json
  
+typesDict = dict()
+typeCount=0
+ 
+def extractData(image,points):
+    print (points)
+    for point in points:
+        x = points[point]['x']
+        y = points[point]['y']
+        
+ 
+def manageImage(singleType, image):
+    print(singleType)
+    for point in singleType:
+        
+        
+def addSingleTypeToDic(singleType):
+    if not singleType in typesDict :
+        typesDict[singleType] = len(typesDict)+1
+
+def manageFile(dataFile):
+    imageFileName = dataFile['file']
+    types = dataFile['type']
+    image = misc.imread(imageFileName)
+    for singleType in types:
+        addSingleTypeToDic(singleType)
+        manageImage(types[singleType], image)
+    print(typesDict)
 
 print("start")
-for image_path in glob.glob("*.jpg"):
-    dataFile = image_path[:-4]+".txt";
-    print (dataFile)
-    with open(dataFile) as f:
-        data = json.load(f);
-        print(len(data["squareYellow"]))
-    image = misc.imread(image_path)
-    print (image.shape)
-    print (image.dtype)
-    print data["squareYellow"].count()(image[0,0])
+with open("data.txt") as f:
+    data = json.load(f);
+for dataFile in data: 
+    manageFile(dataFile)
+        
