aae477844f55066dc5f00ae0acc8716466eb117e
Paolo Achdjian <paolo.achdjian@ext.contactlab.com>
2016-10-18 13:48:21 +0200

    work in progress

--- pollini.py
+++ pollini.py
@@ -6,18 +6,35 @@ import json
  
 typesDict = dict()
 typeCount=0
+HALFSIZE=7
  
-def extractData(image,points):
-    print (points)
-    for point in points:
-        x = points[point]['x']
-        y = points[point]['y']
-        
+def extractData(image, x, y):
+  shape = image.shape
+  left = x - HALFSIZE
+  if left < 0:
+    left=0
+  right = x + HALFSIZE
+  if right > shape[1]:
+    right = shape[1]
+  top = y -HALFSIZE
+  if top < 0:
+    top=0
+  bottom = y + HALFSIZE
+  if bottom > shape[0]:
+    bottom = shape[0]
+  print shape
+  result = ''
+  for xPos in range(left,right+1):
+    for yPos in range(top, bottom+1):
+      result += unichr(image[y][x][0]) + unichr(image[y][x][1]) +unichr(image[y][x][2])
+  return result
  
 def manageImage(singleType, image):
     print(singleType)
     for point in singleType:
-        
+        x = int(singleType[point]['x'])
+        y = int(singleType[point]['y'])
+        extractData(image, x, y);
         
 def addSingleTypeToDic(singleType):
     if not singleType in typesDict :
